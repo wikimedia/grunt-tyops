d704f402c204a5849ab54e21f8e36b4d4fd6852d
James D. Forrester <jforrester@wikimedia.org>
2021-09-07 18:37:26 -0700

    build: Update for the first time in six years(!) (#4)
    * build: Update for the first time in six years(!)
    
    * !fixup Separate section for test output
    
    Co-authored-by: Timo Tijhof <krinklemail@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,14 @@
-/*!
- * Grunt file
- */
+'use strict';
 
-/*jshint node:true */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jscs' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 
 	grunt.initConfig( {
-		jshint: {
-			options: {
-				jshintrc: true
-			},
-			all: [
-				'**/*.js',
-				'!node_modules/**'
-			]
-		},
-		jscs: {
-			all: [
-				'**/*.js',
-				'!node_modules/**'
-			]
-		},
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**'
-			]
-		},
-
-		// Development
-		watch: {
-			files: '**/*',
-			tasks: 'test'
+		eslint: {
+			all: [ '*.js' ]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' ] );
+	grunt.registerTask( 'test', [ 'eslint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
